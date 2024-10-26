dc784c7e62e81b2b9951be14ae6c6ed91d197b13
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-10-26 05:25:28 +0300

    Update

---
+++ scripts/Core/Remove-Tasks.ps1
@@ -0,0 +1,23 @@
+function Remove-ScheduledTasks {
+    param (
+        [Parameter(Mandatory = $true)]
+        [array]$TasksToRemove
+    )
+
+    if ($TasksToRemove -and $TasksToRemove.Count -gt 0) {
+        foreach ($taskNamePattern in $TasksToRemove) {
+            $tasks = Get-ScheduledTask -TaskName "*$taskNamePattern*" -ErrorAction SilentlyContinue
+
+            if ($tasks) {
+                foreach ($task in $tasks) {
+                    Unregister-ScheduledTask -TaskName $task.TaskName -Confirm:$false
+                    Add-Log -Message "$($task.TaskName) Removed" -Level "INFO"
+                }
+            } else {
+                if ($Debug) {
+                    Add-Log -Message "No tasks matching '$taskNamePattern' found" -Level "INFO"
+                }
+            }
+        }
+    }
+}
