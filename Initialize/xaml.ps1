23e12e59c7e4d09c30cd88f8fd4490c59a672bfc
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-10-29 07:00:24 +0300

    Update xaml.ps1

--- Initialize/xaml.ps1
+++ Initialize/xaml.ps1
@@ -239,8 +239,13 @@ $itt.InstallBtn = $itt["window"].FindName("installBtn")
 $itt.ApplyBtn = $itt["window"].FindName("applyBtn")
 $itt.Category = $itt["window"].FindName("category")
 $itt.SearchInput = $itt["window"].FindName("searchInput")
+$itt.SearchIcon = $itt["window"].FindName("SearchIcon")
 $itt.installText = $itt["window"].FindName("installText")
 $itt.installIcon = $itt["window"].FindName("installIcon")
 $itt.applyText = $itt["window"].FindName("applyText")
 $itt.applyIcon = $itt["window"].FindName("applyIcon")
 $itt.QuoteIcon = $itt["window"].FindName("QuoteIcon")
+
+
+
+
