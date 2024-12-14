b6009bbfd04360def0b6db619c449fb07b56de73
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-12-14 06:19:04 +0200

    Update Refresh-Explorer.ps1

--- scripts/Core/Refresh-Explorer.ps1
+++ scripts/Core/Refresh-Explorer.ps1
@@ -1,6 +1,6 @@
 function Refresh-Explorer {
     # Check if explorer is not running and start it if needed
-    Add-Log -Message "Restarting explorer..." -Level "info"
+    Add-Log -Message "Restart explorer." -Level "info"
     Stop-Process -Name explorer -Force
     Start-Sleep -Seconds 1
     if (-not (Get-Process -Name explorer -ErrorAction SilentlyContinue)) {
