a35edcc4fa8f0e9813cb3ffdb468aff80d47657e
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-12-14 06:19:05 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -6889,7 +6889,7 @@ function Native-Downloader {
 }
 function Refresh-Explorer {
     # Check if explorer is not running and start it if needed
-    Add-Log -Message "Restarting explorer..." -Level "info"
+    Add-Log -Message "Restart explorer." -Level "info"
     Stop-Process -Name explorer -Force
     Start-Sleep -Seconds 1
     if (-not (Get-Process -Name explorer -ErrorAction SilentlyContinue)) {
