101c35f53105445e54061b2bfc98475b17175280
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-12-15 10:15:12 +0200

    Update Startup-Log.ps1

--- scripts/Core/Startup-Log.ps1
+++ scripts/Core/Startup-Log.ps1
@@ -234,7 +234,7 @@ function Startup  {
             Write-Host " | |  | |   | |   | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___"
             Write-Host "|___| |_|   |_|   |_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____|"
             Write-Host " Launch Anytime, Anywhere! `n` " 
-            Write-Host " Telegram: https://t.me/bacave" 
+            Write-Host " Telegram: https://t.me/ittemadadel_bot" 
             Write-Host " Discord: https://discord.gg/63m34EE6mX `n` "
             Get-UsersCount
         }
