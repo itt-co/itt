a2e8a3c63ae68871594820cecdec92ace98595f5
Emad Adel <emadadel1920@gmail.com>
2024-12-18 23:15:26 +0200

    Update itt.ps1
    Signed-off-by: Emad Adel <emadadel1920@gmail.com>

--- itt.ps1
+++ itt.ps1
@@ -7327,7 +7327,7 @@ function Startup  {
             Write-Host " Launch Anytime, Anywhere! `n` " 
             Write-Host " Telegram: https://t.me/ittemadadel_bot" 
             Write-Host " Discord: https://discord.gg/63m34EE6mX `n` "
-            #Get-UsersCount
+            Get-UsersCount
         }
         LOG
         PlayMusic
