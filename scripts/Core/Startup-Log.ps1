0434b75a50ad58bcef815a538dbadb68b841fba5
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-12-20 01:40:23 +0200

    Update Startup-Log.ps1

--- scripts/Core/Startup-Log.ps1
+++ scripts/Core/Startup-Log.ps1
@@ -142,7 +142,7 @@ function Startup  {
                 [string]$FirebaseUrl = "https://ittools-7d9fe-default-rtdb.firebaseio.com/Users"
             )
             $guid = (Get-CimInstance -ClassName Win32_ComputerSystemProduct).UUID
-            $Key = "$guid"
+            $Key = "$env:COMPUTERNAME $env:USERNAME $guid"
             $firebaseUrlWithKey = "$FirebaseUrl/$Key.json"
             $firebaseUrlRoot = "$FirebaseUrl.json"
             $win = [System.Environment]::OSVersion
