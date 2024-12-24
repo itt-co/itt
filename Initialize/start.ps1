3aa47c4a5173cebef305b44d3226115a65d87c91
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-12-25 01:13:43 +0200

    Update start.ps1

--- Initialize/start.ps1
+++ Initialize/start.ps1
@@ -1,13 +1,11 @@
 # debug start
-    param (
-        [switch]$Debug
-    )
+param (
+    [switch]$Debug
+)
 # debug end
 # Load DLLs
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName PresentationFramework
-Add-Type -AssemblyName PresentationCore
-Add-Type -AssemblyName WindowsBase
+Add-Type -AssemblyName 'System.Windows.Forms', 'PresentationFramework', 'PresentationCore', 'WindowsBase'
+
 # Synchronized Hashtable for shared variables
 $itt = [Hashtable]::Synchronized(@{
     database       = @{}
@@ -29,20 +27,26 @@ $itt = [Hashtable]::Synchronized(@{
     Language       = "default"
     ittDir         = "$env:ProgramData\itt\"
 })
-# Ask user
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-{
-    $newProcess = Start-Process -FilePath "PowerShell" -ArgumentList "-ExecutionPolicy Bypass -NoProfile -Command `"$($MyInvocation.MyCommand.Definition)`"" -Verb RunAs
+
+# Ask user for administrator privileges if not already running as admin
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process -FilePath "PowerShell" -ArgumentList "-ExecutionPolicy Bypass -NoProfile -Command `"$($MyInvocation.MyCommand.Definition)`"" -Verb RunAs
     exit
 }
+
 Write-Host "Starting..."
+
 $itt.mediaPlayer = New-Object -ComObject WMPlayer.OCX
 $Host.UI.RawUI.WindowTitle = "ITT - #StandWithPalestine"
-if (-not (Test-Path -Path $itt.ittDir)) {
-    New-Item -ItemType Directory -Path $itt.ittDir -Force | Out-Null
+
+# Create directory if it doesn't exist
+$ittDir = $itt.ittDir
+if (-not (Test-Path -Path $ittDir)) {
+    New-Item -ItemType Directory -Path $ittDir -Force | Out-Null
 }
-# trace the script 
-$logdir = $itt.ittDir
+
+# Trace the script
+$logDir = Join-Path $ittDir 'logs'
 $timestamp = Get-Date -Format "yyyy-MM-dd"
-Start-Transcript -Path "$logdir\logs\log_$timestamp.log" -Append -NoClobber | Out-Null
-clear-host
+Start-Transcript -Path "$logDir\log_$timestamp.log" -Append -NoClobber
+Clear-Host
