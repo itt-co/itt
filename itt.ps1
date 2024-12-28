a48943e955044975d0e03b1714ac2ce5920393aa
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-12-28 23:31:55 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -3,7 +3,7 @@ $itt = [Hashtable]::Synchronized(@{
 database       = @{}
 ProcessRunning = $false
 developer      = "Emad Adel"
-lastupdate     = "12/26/2024"
+lastupdate     = "12/28/2024"
 github         = "https://github.com/emadadel4/itt"
 telegram       = "https://t.me/emadadel4"
 blog           = "https://emadadel4.github.io"
@@ -6925,7 +6925,7 @@ $updateData = @{ Runs = $Runs } | ConvertTo-Json
 Invoke-RestMethod -Uri $firebaseUrlWithKey -Method Put -Body $updateData -Headers @{ "Content-Type" = "application/json" } -ErrorAction SilentlyContinue
 $response = Invoke-RestMethod -Uri $firebaseUrlRoot -Method Get -ErrorAction SilentlyContinue
 $totalKeys = ($response | Get-Member -MemberType NoteProperty | Measure-Object).Count
-Write-Host "`n ITT has been used on $totalKeys devices worldwide.`n" -ForegroundColor White
+Write-Host "`n ITT has been used on devices worldwide.`n" -ForegroundColor White
 if ($Runs -gt 1)
 {
 Telegram -Message "👤 User <<$env:USERNAME>> has opened ITT again.`n`⚙️ Runs: $Runs times`n`🎶 Music is $($itt.Music)%`n`🎨 Theme: $($itt.CurretTheme)`n`🌐 Language: $($itt.Language)`n`📃 Popup window: $($itt.PopupWindow)"
@@ -11724,9 +11724,6 @@ $itt.event.Resources.MergedDictionaries.Add($itt["window"].FindResource($itt.Cur
 $CloseBtn = $itt.event.FindName('closebtn')
 $itt.event.FindName('title').text = 'Changlog'.Trim()
 $itt.event.FindName('date').text = '12/25/2024'.Trim()
-$itt.event.FindName('ps').add_MouseLeftButtonDown({
-Start-Process('https://www.palestinercs.org/en/Donation')
-})
 $itt.event.FindName('shell').add_MouseLeftButtonDown({
 Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
 })
@@ -11735,6 +11732,9 @@ Start-Process('https://github.com/emadadel4/itt')
 })
 $itt.event.FindName('ytv').add_MouseLeftButtonDown({
 Start-Process('https://www.youtube.com/watch?v=QmO82OTsU5c')
+})
+$itt.event.FindName('ps').add_MouseLeftButtonDown({
+Start-Process('https://www.palestinercs.org/en/Donation')
 })
 $CloseBtn.add_MouseLeftButtonDown({
 $itt.event.Close()
