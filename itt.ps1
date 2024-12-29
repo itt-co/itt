14b6e0e26101fb3c50e9e0215703c763503dcda4
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-12-29 03:33:17 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -6812,6 +6812,7 @@ text       = $Message
 $Response = Invoke-RestMethod -Uri "$SendMessageUrl/sendMessage" -Method Post -Body $PostBody -ContentType "application/x-www-form-urlencoded"
 }
 catch {
+Add-Log -Message "Your internet connection appears to be slow." -Level "WARNING"
 }
 }
 function PlayMusic {
