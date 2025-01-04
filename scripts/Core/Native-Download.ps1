b63e65f1bded4f4e8680ec96194ab7c6e85f4897
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-01-04 05:31:36 +0200

    Update Native-Download.ps1

--- scripts/Core/Native-Download.ps1
+++ scripts/Core/Native-Download.ps1
@@ -6,32 +6,33 @@ function Native-Downloader {
         [string]$portable,
         [string]$installArgs
     )
-        # Define the destination directory
-        $Destination_Directory = Join-Path -Path "$env:ProgramData\itt\Downloads" -ChildPath $name
-        # Ensure destination directory exists
-        if (-not (Test-Path -Path $Destination_Directory)) {
-            New-Item -ItemType Directory -Path $Destination_Directory -Force | Out-Null
+    # Define the destination directory
+    $Destination_Directory = Join-Path -Path "$env:ProgramData\itt\Downloads" -ChildPath $name
+    # Ensure destination directory exists
+    if (-not (Test-Path -Path $Destination_Directory)) {
+        New-Item -ItemType Directory -Path $Destination_Directory -Force | Out-Null
+    }
+    # Extract file name and ensure we have the file with extension
+    $File = [System.IO.Path]::GetFileName($url)
+    $DownloadPath = Join-Path -Path $Destination_Directory -ChildPath $File
+    $targetPath = Join-Path -Path $Destination_Directory -ChildPath $launcher
+    try {
+        # Start downloading the file
+        Add-Log -Message "Downloading $name using Invoke-WebRequest" -Level "INFO"
+        Invoke-WebRequest -Uri $url -OutFile $DownloadPath -ErrorAction Stop
+        Expand-Archive -Path $DownloadPath -DestinationPath $Destination_Directory -Force -ErrorAction Stop
+    }
+    catch {
+        Write-Error "An error occurred during the download or extraction process: $_"
+    }
+    if ($portable -eq "true") {
+        # Check if the target file exists
+        if (-not (Test-Path -Path $targetPath)) {
+            Add-Log -Message  "Target file '$targetPath' does not exist after extraction." -Level "error"
+            return
         }
-        # Extract file name and ensure we have the file with extension
-        $File = [System.IO.Path]::GetFileName($url)
-        $DownloadPath = Join-Path -Path $Destination_Directory -ChildPath $File
-        $targetPath = Join-Path -Path $Destination_Directory -ChildPath $launcher
-        try {
-            # Start downloading the file
-            Add-Log -Message "Downloading $name using Invoke-WebRequest" -Level "INFO"
-            Invoke-WebRequest -Uri $url -OutFile $DownloadPath -ErrorAction Stop
-            Expand-Archive -Path $DownloadPath -DestinationPath $Destination_Directory -Force -ErrorAction Stop
-        }
-        catch {
-            Write-Error "An error occurred during the download or extraction process: $_"
-        }
-        if($portable -eq "true")
-        {
-            # Check if the target file exists
-            if (-not (Test-Path -Path $targetPath)) {
-                Add-Log -Message  "Target file '$targetPath' does not exist after extraction." -Level "error"
-                return
-            }
+
+        if ($launcher -ne "none" -or "") {
             # Define the path to the desktop shortcut
             $desktopPath = [System.Environment]::GetFolderPath('Desktop')
             $shortcutPath = Join-Path -Path $desktopPath -ChildPath "$name.lnk"
@@ -42,15 +43,16 @@ function Native-Downloader {
                 $shortcut.TargetPath = $targetPath
                 $shortcut.Save()
                 Add-Log -Message "Shortcut created on Destkop" -Level "info"
-            } catch {
+            }
+            catch {
                 Write-Error "Failed to create shortcut. Error: $_"
             }
         }
-        else
-        {
-            Start-Process -FilePath $targetPath -ArgumentList $installArgs -Wait
-            # debug start
-                if($debug) {Write-Host $targetPath}
-            # debug end
-        }
+    }
+    else {
+        Start-Process -FilePath $targetPath -ArgumentList $installArgs -Wait
+        # debug start
+        if ($debug) { Write-Host $targetPath }
+        # debug end
+    }
 }
