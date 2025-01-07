22136cd0f5c199cfe56c952072103c86aca1f175
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-01-07 09:41:44 +0200

    Update Reset-Preferences.ps1

--- scripts/Core/Reset-Preferences.ps1
+++ scripts/Core/Reset-Preferences.ps1
@@ -15,7 +15,7 @@ function Reset-Preferences {
         [string]$PopupWindow = "on",
         [string]$UserTheme = "none"
     )
-    Set-ItemProperty -Path $itt.registryPath  -Name "PopupWindow" -Value $PopupWindow -Force
+    Set-ItemProperty -Path $itt.registryPath  -Name "PopupWindow" -Value 0 -Force
     Set-ItemProperty -Path $itt.registryPath  -Name "Music" -Value $music -Force
     Set-ItemProperty -Path $itt.registryPath  -Name "UserTheme" -Value $UserTheme -Force
     SwitchToSystem
