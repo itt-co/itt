f949cd04fbdd4131206cc3b72d08c2ed80b6f060
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-01-17 06:49:49 +0200

    Update Install.ps1

--- scripts/Install/Install.ps1
+++ scripts/Install/Install.ps1
@@ -11,25 +11,10 @@ function Invoke-Install {
         5. Updates the UI to reflect the installation status.
         6. Executes installation commands for applications using Chocolatey (`Choco`), Winget, or custom download methods.
         7. Updates the UI once the installation is complete and finishes the process.
-        .PARAMETER None
-        This function does not require any parameters.
-        .EXAMPLE
-        Invoke-Install
-        .EXAMPLE
-        To use this function in your script, simply call `Invoke-Install`. This will process the selected items for installation, check user confirmation, and execute the appropriate installation commands based on the type of application.
-        .NOTES
-        - The function starts by checking the count of selected applications using `Get-SelectedItems` with mode "Apps".
-        - It uses a message box to confirm if the user wants to proceed with the installation.
-        - The function updates the UI to reflect the installation status, including a button change and text update.
-        - It installs applications using:
-        - Chocolatey, if a `Choco` identifier is provided.
-        - Winget, if a `Winget` identifier is provided.
-        - Custom download methods (`Start-DownloadAndInstallExe` or `Start-DownloadAndUnzip`) for applications with default settings.
-        - The function handles UI updates and cleanup operations post-installation using `UpdateUI` and `Finish`.
-        - The `Invoke-ScriptBlock` function is used to execute the installation commands asynchronously.
     #>
+    
     if($itt.ProcessRunning) {
-        Message -key "Pleasewait" -icon "Warning" -action "OK"
+        Message -key "Please_wait" -icon "Warning" -action "OK"
         return
     }
     # Get Selected apps
@@ -100,25 +85,8 @@ function Invoke-Apply {
         5. Executes the appropriate tweak operations based on the type of tweak (e.g., commands, registry modifications, Appx package removal, service management).
         6. Updates the UI to reflect the progress and completion of the tweak application.
         7. Finishes the process and logs a message indicating that some tweaks may require a restart.
-        .PARAMETER None
-        This function does not require any parameters.
-        .EXAMPLE
-        Invoke-Apply
-        .EXAMPLE
-        To use this function in your script, call `Invoke-Apply`. This will process the selected tweaks, ask for confirmation, and apply them according to their type.
-        .NOTES
-        - The function starts by checking the count of selected tweaks using `Get-SelectedItems` with mode "Tweaks".
-        - It uses a message box to confirm if the user wants to proceed with applying the tweaks.
-        - The function updates the UI to reflect the application status, including a button change and text update.
-        - It applies tweaks by:
-        - Executing commands specified in the tweak settings.
-        - Modifying or deleting registry entries as per the tweak configuration.
-        - Uninstalling Appx packages if specified.
-        - Disabling services if required.
-        - The function handles UI updates and cleanup operations post-application using `UpdateUI` and `Finish`.
-        - The `Invoke-ScriptBlock` function is used to execute the tweak application commands asynchronously.
-        - Additional logging is performed to track the status of the operation and provide feedback.
     #>
+
     $itt['window'].FindName("TwaeksCategory").SelectedIndex = 0
     $selectedTweaks = Get-SelectedItems -Mode "Tweaks"
     if($itt.ProcessRunning) {
