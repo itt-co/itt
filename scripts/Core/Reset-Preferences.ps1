9de44ea1f02cbffda84525827b09be42f294d908
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-01-28 08:17:08 +0200

    Update Reset-Preferences.ps1

--- scripts/Core/Reset-Preferences.ps1
+++ scripts/Core/Reset-Preferences.ps1
@@ -1,23 +1,13 @@
 function Reset-Preferences {
+
     <#
         .SYNOPSIS
         Resets user preferences for music volume and popup window settings.
-        .DESCRIPTION
-        This function resets the preferences by updating the registry values for the music volume and popup window settings. 
-        It writes the provided values to the specified registry path and switches to the system's default settings.
-        .PARAMETER music
-        Sets the music volume preference. Default is "100".
-        .PARAMETER PopupWindow
-        Sets the popup window visibility. Default is "on".
     #>
-    param (
-        [string]$music = "100",
-        [string]$PopupWindow = "on",
-        [string]$UserTheme = "none"
-    )
+
     Set-ItemProperty -Path $itt.registryPath  -Name "PopupWindow" -Value 0 -Force
-    Set-ItemProperty -Path $itt.registryPath  -Name "Music" -Value $music -Force
-    Set-ItemProperty -Path $itt.registryPath  -Name "UserTheme" -Value $UserTheme -Force
+    Set-ItemProperty -Path $itt.registryPath  -Name "Music" -Value 100 -Force
+    Set-ItemProperty -Path $itt.registryPath  -Name "UserTheme" -Value "none" -Force
     SwitchToSystem
     Message -key "Reopen_itt_again" -icon "Information" -action "OK"
 }
