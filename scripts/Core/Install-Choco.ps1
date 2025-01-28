226bbaf85aee433397b5df5de3db9e6919c46401
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-01-28 08:17:06 +0200

    Update Install-Choco.ps1

--- scripts/Core/Install-Choco.ps1
+++ scripts/Core/Install-Choco.ps1
@@ -2,7 +2,7 @@ function Install-Choco {
 
     if (-not (Get-Command choco -ErrorAction SilentlyContinue))
     {
-        Add-Log -Message "Installing Chocolatey for the first time, It won't take minutes :)" -Level "INFO"
+        Add-Log -Message "Checking dependencies This won't take a minute..." -Level "INFO"
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) *> $null
     }
 }
