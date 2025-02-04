f741e1768be6ed7f71479efd1e17b578b1247d14
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-05 00:31:24 +0200

    Update About-Dev.ps1

--- scripts/UI/About-Dev.ps1
+++ scripts/UI/About-Dev.ps1
@@ -7,11 +7,11 @@ function About {
     $itt["about"].Resources.MergedDictionaries.Add($itt["window"].FindResource($itt.CurretTheme))
     # # Set Events on Click
     $itt.about.FindName('ver').Text = "Last update $($itt.lastupdate)"
-    $itt.about.FindName("telegram").Add_Click({Start-Process($itt.telegram)})
-    $itt.about.FindName("github").Add_Click({Start-Process($itt.github)})
-    $itt.about.FindName("blog").Add_Click({Start-Process($itt.blog)})
-    $itt.about.FindName("yt").Add_Click({Start-Process($itt.youtube)})
-    $itt.about.FindName("coffee").Add_Click({Start-Process($itt.buymeacoffee)})
+    $itt.about.FindName("telegram").Add_Click({Start-Process("https://t.me/emadadel4")})
+    $itt.about.FindName("github").Add_Click({Start-Process("https://github.com/emadadel4/itt")})
+    $itt.about.FindName("blog").Add_Click({Start-Process("https://emadadel4.github.io")})
+    $itt.about.FindName("yt").Add_Click({Start-Process("https://www.youtube.com/@emadadel4")})
+    $itt.about.FindName("coffee").Add_Click({Start-Process("https://buymeacoffee.com/emadadel")})
     # Set data context language
     $itt.about.DataContext = $itt.database.locales.Controls.$($itt.Language)
     # Show window
