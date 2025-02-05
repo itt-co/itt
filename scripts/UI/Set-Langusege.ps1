969830d61f7138b5813aabd7721599dafd3cfcff
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-05 04:36:57 +0200

    Update Set-Langusege.ps1

--- scripts/UI/Set-Langusege.ps1
+++ scripts/UI/Set-Langusege.ps1
@@ -1,25 +1,8 @@
 function System-Default {
 
-    switch($shortCulture)
-    {
-        "ar" { $locale = "ar" }
-        "en" { $locale = "en" }
-        "fr" { $locale = "fr" }
-        "tr" { $locale = "tr" }
-        "zh" { $locale = "zh" }
-        "ko" { $locale = "ko" }
-        "de" { $locale = "de" }
-        "ru" { $locale = "ru" }
-        "es" { $locale = "es" }
-        "ga" { $locale = "ga" }
-        "hi" { $locale = "hi" }
-        "it" { $locale = "it" }
-        # new lang
-        default { $locale = "en" }
-    }
     Set-ItemProperty -Path $itt.registryPath  -Name "locales" -Value "default" -Force
-    $itt["window"].DataContext = $itt.database.locales.Controls.$locale
-    $itt.Language = $locale
+    $itt["window"].DataContext = $itt.database.locales.Controls.$Locales
+    $itt.Language = $Locales
 }
 
 function Set-Language {
