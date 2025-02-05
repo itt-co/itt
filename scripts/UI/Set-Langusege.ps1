ef8d48af694d419a63b86dbdbcde8f95b20bea3a
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-05 07:08:54 +0200

    Update Set-Langusege.ps1

--- scripts/UI/Set-Langusege.ps1
+++ scripts/UI/Set-Langusege.ps1
@@ -1,20 +1,32 @@
 function System-Default {
+    try {
 
-    Set-ItemProperty -Path $itt.registryPath  -Name "locales" -Value "default" -Force
-    $itt["window"].DataContext = $itt.database.locales.Controls.$Locales
-    $itt.Language = $Locales
+        $itt["window"].DataContext = $itt.database.locales.Controls.$shortCulture
+        
+        if (-not $itt["window"].DataContext -or [string]::IsNullOrWhiteSpace($itt["window"].DataContext)) {
+            Add-Log -Message "This language ($shortCulture) is not supported yet, fallback to English" -Level "Info"
+            $itt["window"].DataContext = $itt.database.locales.Controls.en
+            Set-ItemProperty -Path $itt.registryPath  -Name "locales" -Value "default" -Force
+        }
+        else {
+            $itt["window"].DataContext = $itt.database.locales.Controls.$shortCulture
+            Set-ItemProperty -Path $itt.registryPath  -Name "locales" -Value "default" -Force
+        }
+    }
+    catch {
+        Write-Host "An error occurred: $_"
+    }
 }
+
 
 function Set-Language {
     param (
         [string]$lang
     )
-    if($lang -eq "default")
-    {
+    if ($lang -eq "default") {
         System-Default
     }
-    else
-    {
+    else {
         # Set registry value for the language
         $itt.Language = $lang
         Set-ItemProperty -Path $itt.registryPath  -Name "locales" -Value "$lang" -Force
