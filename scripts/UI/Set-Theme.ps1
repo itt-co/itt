4b172443a05dd09d084bb595b7d20c93cdc2a677
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-06 10:55:17 +0200

    Update Set-Theme.ps1

--- scripts/UI/Set-Theme.ps1
+++ scripts/UI/Set-Theme.ps1
@@ -1,29 +1,29 @@
 function SwitchToSystem {
+
+    <#
+        .SYNOPSIS
+        Functions to manage application theme settings.
+
+        .DESCRIPTION
+        SwitchToSystem resets the theme to the system default and applies the appropriate resource.
+        Set-Theme applies a user-defined theme and updates the registry accordingly.
+    #>
+
     try {
-        Set-ItemProperty -Path $itt.registryPath  -Name "Theme" -Value "default" -Force
-        $AppsTheme = (Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme")
-        switch ($AppsTheme) {
-            "0" {
-                $itt['window'].Resources.MergedDictionaries.Add($itt['window'].FindResource("Dark"))
-            }
-            "1" {
-                $itt['window'].Resources.MergedDictionaries.Add($itt['window'].FindResource("Light"))
-            }
-            Default {
-                Write-Host "Unknown theme value: $AppsTheme"
-            }
-        }
+        Set-ItemProperty -Path $itt.registryPath -Name "Theme" -Value "default" -Force
+        $theme = if ($AppsTheme -eq "0") { "Dark" } elseif ($AppsTheme -eq "1") { "Light" } else { Write-Host "Unknown theme: $AppsTheme"; return }
+        $itt['window'].Resources.MergedDictionaries.Add($itt['window'].FindResource($theme))
     }
-    catch {
-        Write-Host "Error occurred: $_"
-    }
+    catch { Write-Host "Error: $_" }
 }
+
 function Set-Theme {
-    param (
-        [string]$Theme
-    )
-    $itt['window'].Resources.MergedDictionaries.Clear()
-    $itt['window'].Resources.MergedDictionaries.Add($itt['window'].FindResource("$Theme"))
-    Set-ItemProperty -Path $itt.registryPath -Name "Theme" -Value "$Theme" -Force
-    $itt.Theme = $Theme
+    param ([string]$Theme)
+    try {
+        $itt['window'].Resources.MergedDictionaries.Clear()
+        $itt['window'].Resources.MergedDictionaries.Add($itt['window'].FindResource($Theme))
+        Set-ItemProperty -Path $itt.registryPath -Name "Theme" -Value $Theme -Force
+        $itt.Theme = $Theme
+    }
+    catch { Write-Host "Error: $_" }
 }
