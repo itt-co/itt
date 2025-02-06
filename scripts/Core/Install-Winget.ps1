0486f4de7f926aceb589bfbba502c3542c55d2d4
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-06 08:51:48 +0200

    Fix & Better way installing Winget.

--- scripts/Core/Install-Winget.ps1
+++ scripts/Core/Install-Winget.ps1
@@ -1,74 +1,42 @@
 function Install-Winget {
+
     <#
         .SYNOPSIS
-        Installs the Windows Package Manager (`winget`) and its dependencies.
+        Installs Winget on Windows systems that support it.
+
         .DESCRIPTION
-        This function installs `winget` by first checking if it is already installed on the system. If not, it downloads and installs the necessary dependencies:
-        1. `Microsoft.VCLibs` package.
-        2. `Microsoft.UI.Xaml` package.
-        3. The `Microsoft Store App Installer` which includes `winget`.
-        It also ensures that the `winget` path is added to the system environment variables if it is not already present.
-        .PARAMETER None
-        This function does not require any parameters.
-        .EXAMPLE
-        Install-Winget
-        .EXAMPLE
-        If you want to run this function in a PowerShell script, just call `Install-Winget`. This will download and install `winget` and its prerequisites if they are not already installed on the system.
-        .NOTES
-        - The function determines the OS architecture (32-bit or 64-bit) and selects the appropriate package URLs for `Microsoft.VCLibs` and `Microsoft.UI.Xaml`.
-        - It checks for the existence of `winget` using `Get-Command`. If `winget` is found, the function exits without making changes.
-        - It downloads the necessary `.appx` packages and installs them using `Add-AppxPackage`.
-        - It downloads and installs the `Microsoft Store App Installer` which includes `winget`.
-        - The function attempts to add the `winget` path to the system environment variables if it is not already included.
-        - The function includes error handling to throw a custom exception if the installation of prerequisites fails.
+        This function checks if Winget is installed on the system. If not, it verifies the system's architecture and Windows version to ensure compatibility. It 
+        then downloads and installs the necessary dependencies and Winget itself.
     #>
-    $versionVCLibs = "14.00"
-    $versionUIXamlMinor = "2.8"
-    $versionUIXamlPatch = "2.8.6"
-    function Get-OSArchitecture {
-    $is64Bit = $env:PROCESSOR_ARCHITEW6432 -eq "AMD64"
-    $architecture = if ($is64Bit) { "64-bit" } else { "32-bit" }
-    return $architecture
+
+    if(Get-Command winget -ErrorAction SilentlyContinue) {return}
+    $ComputerInfo = Get-ComputerInfo -ErrorAction Stop
+    $arch = [int](($ComputerInfo).OsArchitecture -replace '\D', '')
+
+    if ($ComputerInfo.WindowsVersion -lt "1809") {
+        Add-Log -Message "Winget is not supported on this version of Windows Upgrade to 1809 or newer." -Level "info" 
+        return
     }
-    if (Get-OSArchitecture -eq "64-bit") {
-    $fileVCLibs = "https://aka.ms/Microsoft.VCLibs.x64.${versionVCLibs}.Desktop.appx"
-    $fileUIXaml = "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v${versionUIXamlPatch}/Microsoft.UI.Xaml.${versionUIXamlMinor}.x64.appx"
-    } 
-    else
-    {
-    $fileVCLibs = "https://aka.ms/Microsoft.VCLibs.x86.${versionVCLibs}.Desktop.appx"
-    $fileUIXaml = "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v${versionUIXamlPatch}/Microsoft.UI.Xaml.${versionUIXamlMinor}.x86.appx"
+
+    $VCLibs = "https://aka.ms/Microsoft.VCLibs.x$arch.14.00.Desktop.appx"
+    $UIXaml = "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x$arch.appx"
+    $WingetLatset = "https://aka.ms/getwinget"
+
+    try {
+        
+        Add-Log -Message "Installing Winget... This might take several minutes" -Level "info"
+        Start-BitsTransfer -Source $VCLibs -Destination "$env:TEMP\Microsoft.VCLibs.Desktop.appx"
+        Start-BitsTransfer -Source $UIXaml -Destination "$env:TEMP\Microsoft.UI.Xaml.appx"
+        Start-BitsTransfer -Source $WingetLatset -Destination "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+
+        Add-AppxPackage "$env:TEMP\Microsoft.VCLibs.Desktop.appx"
+        Add-AppxPackage "$env:TEMP\Microsoft.UI.Xaml.appx"
+        Add-AppxPackage "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+        Start-Sleep -Seconds 1
+        Add-Log -Message "Successfully installed Winget. Continuing to install selected apps..." -Level "info"
+        return
     }
-    Try {
-        if (Get-Command winget -ErrorAction SilentlyContinue) {
-            Write-Host "winget is installed on this system."
-            return
-        } 
-        Write-Host "Downloading Microsoft.VCLibs Dependency..."
-        Invoke-WebRequest -Uri $fileVCLibs -OutFile $ENV:TEMP\Microsoft.VCLibs.x64.Desktop.appx
-        Write-Host "Downloading Microsoft.UI.Xaml Dependency...`n"
-        Invoke-WebRequest -Uri $fileUIXaml -OutFile $ENV:TEMP\Microsoft.UI.Xaml.x64.appx
-        # Install Microsoft.VCLibs
-        Add-AppxPackage -Path "$ENV:TEMP\Microsoft.VCLibs.x64.Desktop.appx"
-        # Install Microsoft.UI.Xaml
-        Add-AppxPackage -Path "$ENV:TEMP\Microsoft.UI.Xaml.x64.appx"
-        $msiPath = "$env:TEMP\winget.msixbundle"
-        $url = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-        Invoke-WebRequest -Uri $url -OutFile $msiPath
-        # Install the Microsoft Store App Installer silently
-        Add-AppxPackage -Path $msiPath -ErrorAction Stop
-        # Wait for the installation to complete
-        Start-Sleep -Seconds 2
-        # Add winget to the system environment variable 'Path' if not already present
-        $wingetPath = "$env:ProgramFiles\WindowsApps\Microsoft.DesktopAppInstaller_1.11.12371.0_x64__8wekyb3d8bbwe"
-        $pathVariable = [Environment]::GetEnvironmentVariable("Path", "Machine")
-        if (-not ($pathVariable -split ";" | Where-Object {$_ -eq $wingetPath})) {
-            $newPath = "$pathVariable;$wingetPath"
-            [Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
-        }
-        $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-    }
-    Catch {
-        throw [WingetFailedInstall]::new('Failed to install prerequisites')
+    catch {
+        Write-Error "Failed to install $_"
     }
 }
