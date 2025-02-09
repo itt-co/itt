3ad93372c383c9b0b201f614cb38b02ab57df3dd
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-10 00:42:35 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -6512,6 +6512,7 @@ $response = Invoke-RestMethod -Uri $UsersCount -Method Get
 return $response
 }
 function PlayMusic {
+$ST = Invoke-RestMethod -Uri "https://raw.githubusercontent.com/emadadel4/itt/refs/heads/main/static/Database/OST.json" -Method Get
 function PlayAudio($track) {
 $mediaItem = $itt.mediaPlayer.newMedia($track)
 $itt.mediaPlayer.currentPlaylist.appendItem($mediaItem)
@@ -6519,9 +6520,9 @@ $itt.mediaPlayer.controls.play()
 }
 function GetShuffledTracks {
 switch ($itt.Date.Month, $itt.Date.Day) {
-{ $_ -eq 9, 1 } { return $itt.database.OST.Favorite | Get-Random -Count $itt.database.OST.Favorite.Count }
-{ $_ -eq 10, 6 -or $_ -eq 10, 7 } { return $itt.database.OST.Otobers | Get-Random -Count $itt.database.OST.Otobers.Count }
-default { return $itt.database.OST.Tracks | Get-Random -Count $itt.database.OST.Tracks.Count }
+{ $_ -eq 9, 1 } { return $ST.Favorite | Get-Random -Count $ST.Favorite.Count }
+{ $_ -eq 10, 6 -or $_ -eq 10, 7 } { return $itt.database.OST.Otobers | Get-Random -Count $ST.Otobers.Count }
+default { return $ST.Tracks | Get-Random -Count $ST.Tracks.Count }
 }
 }
 function PlayPreloadedPlaylist {
@@ -6536,62 +6537,26 @@ Start-Sleep -Milliseconds 100
 PlayPreloadedPlaylist
 }
 function Quotes {
-$jsonFilePath = $itt.database.Quotes
-function ShuffleArray {
-param (
-[array]$Array
-)
-$count = $Array.Count
-for ($i = $count - 1; $i -ge 0; $i--) {
-$randomIndex = Get-Random -Minimum 0 -Maximum $count
-$temp = $Array[$i]
-$Array[$i] = $Array[$randomIndex]
-$Array[$randomIndex] = $temp
+function Get-Quotes {
+(Invoke-RestMethod "https://raw.githubusercontent.com/emadadel4/itt/refs/heads/main/static/Database/Quotes.json").Quotes | Sort-Object { Get-Random }
 }
-return $Array
-}
-function Get-QuotesFromJson {
-$jsonContent = $jsonFilePath
-return $jsonContent.Quotes
-}
-$shuffledQuotes = ShuffleArray -Array (Get-QuotesFromJson)
-function Show-WelcomeText {
+function Show-Quote($text, $icon) {
 $itt.Quotes.Dispatcher.Invoke([Action]{
-$itt.QuoteIcon.Text = ""
-$itt.Quotes.Text = $itt.database.locales.Controls.$($itt.Language).welcome
+$itt.QuoteIcon.Text = $icon
+$itt.Quotes.Text = $text
 })
 }
-Show-WelcomeText
-Start-Sleep -Seconds 28
+Show-Quote $itt.database.locales.Controls.$($itt.Language).welcome ""
+Start-Sleep 20
+Show-Quote "Can you uncover the hidden secret? Dive into the source code, be the first to discover the feature, and integrate it into the tool" ""
+Start-Sleep 18
+$iconMap = @{quote=""; info=""; music=""; Cautton=""; default=""}
 do {
-foreach ($quote in $shuffledQuotes) {
-$itt.Quotes.Dispatcher.Invoke([Action]{
-switch ($quote.type) {
-"quote" {
-$itt.QuoteIcon.Text = ""
-}
-"info" {
-$itt.QuoteIcon.Text = ""
-}
-"music" {
-$itt.QuoteIcon.Text = ""
-}
-"Cautton"
-{
-$itt.QuoteIcon.Text = ""
-}
-Default {
-$itt.QuoteIcon.Text = ""
-}
-}
-$quoteText = if ($quote.name) {
-"`“$($quote.text)`” ― $($quote.name)"
-} else {
-"`“$($quote.text)`”"
-}
-$itt.Quotes.Text = $quoteText
-})
-Start-Sleep -Seconds 18
+foreach ($q in Get-Quotes) {
+$icon = if ($iconMap.ContainsKey($q.type)) { $iconMap[$q.type] } else { $iconMap.default }
+$text = "`“$($q.text)`”" + $(if ($q.name) { " ― $($q.name)" } else { "" })
+Show-Quote $text $icon
+Start-Sleep 20
 }
 } while ($true)
 }
