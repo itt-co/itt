d436bc67befbf6e89c8553d879191601deea0f3b
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-10 00:08:08 +0200

    Update Show-Event.ps1

--- scripts/UI/Show-Event.ps1
+++ scripts/UI/Show-Event.ps1
@@ -1,36 +1,27 @@
 function Show-Event {
-    #if($itt.PopupWindow -eq "off") {return}   
+    # if ($itt.PopupWindow -eq "off") { return }
     [xml]$event = $EventWindowXaml
-    $EventWindowReader = (New-Object System.Xml.XmlNodeReader $event)
-    $itt.event = [Windows.Markup.XamlReader]::Load($EventWindowReader)
+    $itt.event = [Windows.Markup.XamlReader]::Load((New-Object System.Xml.XmlNodeReader $event))
     $itt.event.Resources.MergedDictionaries.Add($itt["window"].FindResource($itt.Theme))
-    $CloseBtn = $itt.event.FindName('closebtn')
+
+    $itt.event.FindName('closebtn').add_MouseLeftButtonDown({ $itt.event.Close() })
+    $itt.event.FindName('DisablePopup').add_MouseLeftButtonDown({ DisablePopup; $itt.event.Close() })
+
     #{title}
     #{contorlshandler}
-    $CloseBtn.add_MouseLeftButtonDown({
-        $itt.event.Close()
+    
+    $itt.event.Add_PreViewKeyDown({
+        if ($_.Key -eq "Escape") { $itt.event.Close() }
     })
-    $itt.event.FindName('DisablePopup').add_MouseLeftButtonDown({
-        DisablePopup
-        $itt.event.Close()
-    })
-    # Escape to Close window
-    $KeyEvents = {
-        if ($_.Key -eq "Escape") {
-            $itt.event.Close()
-        }
-    }
-    $itt.event.Add_PreViewKeyDown($KeyEvents)
+
     # Calculate timestamp
-    $storedDateStr = $itt.event.FindName('date').text
-    $storedDate = [datetime]::ParseExact($storedDateStr, 'MM/dd/yyyy', $null)
-    $currentDate = Get-Date
-    $daysElapsed = ($currentDate - $storedDate).Days
-    # show popup on update event, ignore popup if user launched it using -i command
-    if ($daysElapsed -lt 1 -or $itt.PopupWindow -eq "0" -and -not $i) {
+    $storedDate = [datetime]::ParseExact($itt.event.FindName('date').Text, 'MM/dd/yyyy', $null)
+    $daysElapsed = (Get-Date) - $storedDate
+    if (($daysElapsed.Days -lt 1) -or (($itt.PopupWindow -eq "0") -and (-not $i))) {
         $itt.event.ShowDialog() | Out-Null
     }
 }
+
 function DisablePopup {
-    Set-ItemProperty -Path $itt.registryPath  -Name "PopupWindow" -Value 1 -Force
+    Set-ItemProperty -Path $itt.registryPath -Name "PopupWindow" -Value 1 -Force
 }
