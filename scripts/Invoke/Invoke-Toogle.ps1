51274d057a9dcb1adbc68c9701c5282eecfd5606
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-10 10:17:28 +0200

    Update Invoke-Toogle.ps1

--- scripts/Invoke/Invoke-Toogle.ps1
+++ scripts/Invoke/Invoke-Toogle.ps1
@@ -1,37 +1,30 @@
-function Invoke-Toogle {
+function Invoke-Toggle {
+
     <#
         .SYNOPSIS
-        Toggles various system settings based on the specified debug parameter.
+        Toggles various system settings based on the provided debug parameter.
+
         .DESCRIPTION
-        The `Invoke-Toogle` function manages system settings such as showing file extensions, toggling dark mode, showing hidden files, and managing keyboard settings. It determines the current state of the setting and invokes the appropriate functions to toggle or apply the changes.
-        .PARAMETER debug
-        A string specifying the setting to toggle. The value should match one of the predefined toggle names, such as "ToggleShowExt" or "ToggleDarkMode". The function uses this parameter to determine which setting to check and change.
+        The `Invoke-Toggle` function dynamically toggles system settings such as dark mode, file extensions visibility, 
+        and performance options. It determines the appropriate action based on the input parameter and executes 
+        the corresponding function.
+
         .EXAMPLE
-        Invoke-Toogle -debug "ToggleShowExt"
-        .EXAMPLE
-        Invoke-Toogle -debug "ToggleDarkMode"
-        .NOTES
-        - The function uses a `Switch` statement to handle different toggle actions based on the `$debug` parameter.
-        - It calls `Get-ToggleStatus` to retrieve the current state of the specified setting and then calls the corresponding function to apply the toggle.
-        - The `Invoke-ShowFile-Extensions`, `Invoke-DarkMode`, `Invoke-ShowFile`, `Invoke-NumLock`, and `Invoke-StickyKeys` functions are used to modify the respective settings.
-        - The debug parameter is primarily used for testing and development purposes. Uncommenting the `Write-Host` line can provide additional output for debugging.
+        Invoke-Toggle -Debug "darkmode"
+        Toggles the system's dark mode setting.
     #>
-    Param ([string]$debug)
-    Switch -Wildcard ($debug){
-        "showfileextensions" {Invoke-ShowFile-Extensions $(Get-ToggleStatus showfileextensions)}
-        "darkmode" {Invoke-DarkMode $(Get-ToggleStatus darkmode)}
-        "showsuperhidden" {Invoke-ShowFile $(Get-ToggleStatus showsuperhidden)}
-        "numlook" {Invoke-NumLock $(Get-ToggleStatus numlook)}
-        "stickykeys" {Invoke-StickyKeys $(Get-ToggleStatus stickykeys)}
-        "mouseacceleration" {Invoke-MouseAcceleration $(Get-ToggleStatus mouseacceleration)}
-        "endtaskontaskbarwindows11" {Invoke-TaskbarEnd $(Get-ToggleStatus endtaskontaskbarwindows11)}
-        "clearpagefileatshutdown" {Invoke-ClearPageFile $(Get-ToggleStatus clearpagefileatshutdown)}
-        "autoendtasks" {Invoke-AutoEndTasks $(Get-ToggleStatus autoendtasks)}
-        "performanceoptions" {Invoke-PerformanceOptions $(Get-ToggleStatus performanceoptions)}
-        "launchtothispc" {Invoke-LaunchTo $(Get-ToggleStatus launchtothispc)}
-        "disableautomaticdriverinstallation" {Invoke-DisableAutoDrivers $(Get-ToggleStatus disableautomaticdriverinstallation)}
+
+    Param ([string]$Debug)
+
+    $toggleActions = @{
+        "showfileextensions" = "Invoke-ShowFile-Extensions"; "darkmode" = "Invoke-DarkMode"
+        "showsuperhidden" = "Invoke-ShowFile"; "numlock" = "Invoke-NumLock"
+        "stickykeys" = "Invoke-StickyKeys"; "mouseacceleration" = "Invoke-MouseAcceleration"
+        "endtaskontaskbarwindows11" = "Invoke-TaskbarEnd"; "clearpagefileatshutdown" = "Invoke-ClearPageFile"
+        "autoendtasks" = "Invoke-AutoEndTasks"; "performanceoptions" = "Invoke-PerformanceOptions"
+        "launchtothispc" = "Invoke-LaunchTo"; "disableautomaticdriverinstallation" = "Invoke-DisableAutoDrivers"
     }
-    # debug start
-        Add-Log -Message $debug -Level "debug"
-    # debug end
+
+    if ($toggleActions[$Debug.ToLower()]) { & $toggleActions[$Debug.ToLower()] $(Get-ToggleStatus $Debug) }
+    else { Write-Warning "Invalid toggle: $Debug"; Add-Log -Message "Invalid toggle: $Debug" -Level "warning" }
 }
