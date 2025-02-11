3e09ae9ddf3d5efe88504f81ad8f09da3babf7d8
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-11 12:32:45 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -6547,12 +6547,14 @@ if ($newValue -eq 1) { NewUser }
 Write-Host "`n ITT has been used on $(GetCount) devices worldwide.`n" -ForegroundColor White
 }
 function LOG {
-Write-Host "██╗████████╗████████╗ itt @emadadel4"
-Write-Host "██║╚══██╔══╝╚══██╔══╝ https://t.me/emadadel4"
-Write-Host "██║   ██║      ██║    https://github.com/emadadel4/itt"
-Write-Host "██║   ██║      ██║   "
-Write-Host "██║   ██║      ██║   "
-Write-Host "╚═╝   ╚═╝      ╚═╝   "
+Write-Host "                                    "
+Write-Host "                                    "
+Write-Host "  ██╗████████╗████████╗ itt @emadadel4"
+Write-Host "  ██║╚══██╔══╝╚══██╔══╝ https://t.me/emadadel4"
+Write-Host "  ██║   ██║      ██║    https://github.com/emadadel4/itt"
+Write-Host "  ██║   ██║      ██║   "
+Write-Host "  ██║   ██║      ██║   "
+Write-Host "  ╚═╝   ╚═╝      ╚═╝   "
 Welcome
 }
 LOG
@@ -11497,23 +11499,23 @@ $itt.event.FindName('closebtn').add_MouseLeftButtonDown({ $itt.event.Close() })
 $itt.event.FindName('DisablePopup').add_MouseLeftButtonDown({ DisablePopup; $itt.event.Close() })
 $itt.event.FindName('title').text = 'Changelog'.Trim()
 $itt.event.FindName('date').text = '01/31/2025'.Trim()
-$itt.event.FindName('ps').add_MouseLeftButtonDown({
-Start-Process('https://www.palestinercs.org/en/Donation')
-})
 $itt.event.FindName('ytv').add_MouseLeftButtonDown({
 Start-Process('https://www.youtube.com/watch?v=QmO82OTsU5c')
-})
-$itt.event.FindName('preview2').add_MouseLeftButtonDown({
-Start-Process('https://github.com/emadadel4/itt')
-})
-$itt.event.FindName('esg').add_MouseLeftButtonDown({
-Start-Process('https://github.com/emadadel4/itt')
 })
 $itt.event.FindName('preview').add_MouseLeftButtonDown({
 Start-Process('https://github.com/emadadel4/itt')
 })
 $itt.event.FindName('shell').add_MouseLeftButtonDown({
 Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
+})
+$itt.event.FindName('esg').add_MouseLeftButtonDown({
+Start-Process('https://github.com/emadadel4/itt')
+})
+$itt.event.FindName('ps').add_MouseLeftButtonDown({
+Start-Process('https://www.palestinercs.org/en/Donation')
+})
+$itt.event.FindName('preview2').add_MouseLeftButtonDown({
+Start-Process('https://github.com/emadadel4/itt')
 })
 $itt.event.Add_PreViewKeyDown({
 if ($_.Key -eq "Escape") { $itt.event.Close() }
