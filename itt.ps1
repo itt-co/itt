8298c4969f046e34b1649a1fbf297cc62782c0c8
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-12 01:02:01 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -7092,13 +7092,13 @@ function About {
 $childWindowReader = (New-Object System.Xml.XmlNodeReader $about)
 $itt.about = [Windows.Markup.XamlReader]::Load($childWindowReader)
 $itt['about'].Resources.MergedDictionaries.Clear()
-$itt["about"].Resources.MergedDictionaries.Add($itt["window"].FindResource($($itt.Theme)))
+$itt["about"].Resources.MergedDictionaries.Add($itt["window"].FindResource($itt.Theme))
 $itt.about.FindName('ver').Text = "Last update $($itt.lastupdate)"
-$itt.about.FindName("telegram").Add_Click({Start-Process("https://t.me/emadadel4")})
-$itt.about.FindName("github").Add_Click({Start-Process("https://github.com/emadadel4/itt")})
-$itt.about.FindName("blog").Add_Click({Start-Process("https://emadadel4.github.io")})
-$itt.about.FindName("yt").Add_Click({Start-Process("https://www.youtube.com/@emadadel4")})
-$itt.about.FindName("coffee").Add_Click({Start-Process("https://buymeacoffee.com/emadadel")})
+$itt.about.FindName("telegram").Add_Click({ Start-Process("https://t.me/emadadel4") })
+$itt.about.FindName("github").Add_Click({ Start-Process("https://github.com/emadadel4/itt") })
+$itt.about.FindName("blog").Add_Click({ Start-Process("https://emadadel4.github.io") })
+$itt.about.FindName("yt").Add_Click({ Start-Process("https://www.youtube.com/@emadadel4") })
+$itt.about.FindName("coffee").Add_Click({ Start-Process("https://buymeacoffee.com/emadadel") })
 $itt.about.DataContext = $itt.database.locales.Controls.$($itt.Language)
 $itt.about.ShowDialog() | Out-Null
 }
