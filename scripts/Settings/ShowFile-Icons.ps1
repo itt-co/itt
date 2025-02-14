7b2483fa4ef187af06d0755daa136e2c0ec7eb63
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-14 23:41:00 +0200

    Create ShowFile-Icons.ps1

---
+++ scripts/Settings/ShowFile-Icons.ps1
@@ -0,0 +1,32 @@
+function Invoke-ShowFile-Icons {
+
+    <#
+        .SYNOPSIS
+        Toggles the visibility of thmbnails in Windows Explorer.
+    #>
+
+    param ($Enabled, $Name = "IconsOnly", $Path = "HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced")
+
+    Try {
+        if ($Enabled -eq $false) {
+            $value = 0
+            Add-Log -Message "ON" -Level "Apply"
+        }
+        else {
+            $value = 1
+            Add-Log -Message "OFF" -Level "Disabled"
+        }
+        Set-ItemProperty -Path $Path -Name $Name -Value $value -ErrorAction Stop
+        Refresh-Explorer
+    }
+    Catch [System.Security.SecurityException] {
+        Write-Warning "Unable to set $Path\$Name to $Value due to a Security Exception"
+    }
+    Catch [System.Management.Automation.ItemNotFoundException] {
+        Write-Warning $psitem.Exception.ErrorRecord
+    }
+    Catch {
+        Write-Warning "Unable to set $Name due to unhandled exception"
+        Write-Warning $psitem.Exception.StackTrace
+    }
+}
