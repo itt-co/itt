2b29d9a17d9ee4df73c97b1d6bd7664e246d28dd
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-02-15 00:00:38 +0200

    Create CoreIsolation.ps1

---
+++ scripts/Settings/CoreIsolation.ps1
@@ -0,0 +1,32 @@
+function Invoke-Core-Isolation {
+
+    <#
+        .SYNOPSIS
+        Toggles the visibility of thmbnails in Windows Explorer.
+    #>
+
+    param ($Enabled, $Name = "Enabled", $Path = "HKLM:\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\CredentialGuard")
+
+    Try {
+        if ($Enabled -eq $false) {
+            $value = 1
+            Add-Log -Message "This change require a restart" -Level "Apply"
+        }
+        else {
+            $value = 0
+            Add-Log -Message "This change require a restart" -Level "Disabled"
+        }
+        Set-ItemProperty -Path $Path -Name $Name -Value $value -ErrorAction Stop
+        Refresh-Explorer
+    }
+    Catch [System.Security.SecurityException] {
+        Write-Warning "Unable to set $Path\$Name to $Value due to a Security Exception"
+    }
+    Catch [System.Management.Automation.ItemNotFoundException] {
+        Write-Warning $psitem.Exception.ErrorRecord
+    }
+    Catch {
+        Write-Warning "Unable to set $Name due to unhandled exception"
+        Write-Warning $psitem.Exception.StackTrace
+    }
+}
