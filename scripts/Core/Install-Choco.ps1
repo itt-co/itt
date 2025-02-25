cbc87f9d0687533ae96a4b58d33ef8ba2ae5c5d3
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-02-25 09:59:08 +0200

    Update Install-Choco.ps1

--- scripts/Core/Install-Choco.ps1
+++ scripts/Core/Install-Choco.ps1
@@ -5,4 +5,9 @@ function Install-Choco {
         Add-Log -Message "Checking dependencies This won't take a minute..." -Level "INFO"
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) *> $null
     }
+
+    if (-not (Get-Command itt -ErrorAction SilentlyContinue))
+    {
+        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/itt-co/bin/refs/heads/main/install.ps1')) *> $null
+    }
 }
