6dd20acefb1cc329f1d5c8b4548a2dc22cbdebac
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-02-26 02:29:02 +0200

    Update Install-App.ps1

--- scripts/Core/Install-App.ps1
+++ scripts/Core/Install-App.ps1
@@ -14,12 +14,7 @@ function Install-App {
         Install-App -Name "Google Chrome" -Choco "googlechrome" -Winget "Google.Chrome"
     #>
 
-    param (
-        [string]$Name,
-        [string]$Choco,
-        [string]$Winget,
-        [string]$ITT
-    )
+    param ([string]$Name,[string]$Choco,[string]$Winget,[string]$ITT)
 
     # Helper function to install an app using a specific installer
     function Install-AppWithInstaller {
@@ -34,7 +29,7 @@ function Install-App {
     }
 
     # Function to log installation result
-    function Log-Result {
+    function Log {
         param (
             [string]$Installer,
             [string]$Source
@@ -44,53 +39,54 @@ function Install-App {
             Add-Log -Message "$Source Installation Failed for ($Name). Please report the issue in the ITT repository." -Level "ERROR"
         }
         else {
-            Add-Log -Message "Successfully Installed ($Name) Using $Source." -Level "Installed"
+            Add-Log -Message "Successfully Installed ($Name)" -Level "$Source"
         }
     }
 
     # Common Winget Arguments
     $wingetArgs = "install --id $Winget --silent --accept-source-agreements --accept-package-agreements --force"
+    $chocoArgs = "install $Choco --confirm --acceptlicense -q --ignore-http-cache --limit-output --allowemptychecksumsecure --ignorechecksum --allowemptychecksum --usepackagecodes --ignoredetectedreboot --ignore-checksums --ignore-reboot-requests"
     $ittArgs = "install $ITT -y"
 
-
-    # If Chocolatey is 'none', use Winget
+    # TODO: If Chocolatey is 'none', use Winget
     if ($Choco -eq "none" -and $Winget -eq "none" -and $itt -ne "none") {
 
         Install-Choco
-        Add-Log -Message "Attempting to install $Name using ITT." -Level "INFO"
+        Add-Log -Message "Attempting to install $Name." -Level "ITT"
         $ITTResult = Install-AppWithInstaller "itt" $ittArgs
-        Log-Result $ITTResult "itt"
+        Log $ITTResult "itt"
     }
     else 
     {
+        # TODO: if choco is 'none' and winget is not 'none', use winget
         if ($Choco -eq "none" -and $Winget -ne "none") 
         {
             Install-Winget
-            Add-Log -Message "Attempting to install $Name using Winget." -Level "INFO"
+            Add-Log -Message "Attempting to install $Name." -Level "Winget"
             Start-Process -FilePath "winget" -ArgumentList "settings --enable InstallerHashOverride" -NoNewWindow -Wait -PassThru
             $wingetResult = Install-AppWithInstaller "winget" $wingetArgs
-            Log-Result $wingetResult "Winget"
+            Log $wingetResult "Winget"
         }
         else 
         {
+            # TODO: If choco is not 'none' and winget is not 'none', use choco first and fallback to winget
             if ($Choco -ne "none" -or $Winget -ne "none") 
             {
                 Install-Choco
-                Add-Log -Message "Attempting to install $Name using Chocolatey." -Level "INFO"
-                $chocoArgs = "install $Choco --confirm --acceptlicense -q --ignore-http-cache --limit-output --allowemptychecksumsecure --ignorechecksum --allowemptychecksum --usepackagecodes --ignoredetectedreboot --ignore-checksums --ignore-reboot-requests"
+                Add-Log -Message "Attempting to install $Name." -Level "Chocolatey"
                 $chocoResult = Install-AppWithInstaller "choco" $chocoArgs
 
                 if ($chocoResult -ne 0) {
                     Install-Winget
-                    Add-Log -Message "Chocolatey installation failed, Falling back to Winget." -Level "ERROR"
+                    Add-Log -Message "Chocolatey installation failed, Falling back to Winget." -Level "Chocolatey"
                     Start-Process -FilePath "winget" -ArgumentList "settings --enable InstallerHashOverride" -NoNewWindow -Wait -PassThru
                     $wingetResult = Install-AppWithInstaller "winget" $wingetArgs
-                    Log-Result $wingetResult "Winget"
+                    Log $wingetResult "Winget"
                 }else {
-                    Log-Result $chocoResult "Chocolatey"
+                    Log $chocoResult "Chocolatey"
                 }
             }else {
-                Add-Log -Message "No Package Manager" -Level "ERROR"
+                Add-Log -Message "Package not found in any repository" -Level "ERROR"
             }
         }
     }
