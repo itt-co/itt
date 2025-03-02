071d71a2fb39cf6ed62d23f3c98c96dde0a8488b
itt-co <167635892+itt-co@users.noreply.github.com>
2025-03-02 21:42:41 +0000

    update

--- itt.ps1
+++ itt.ps1
@@ -7247,8 +7247,8 @@ $itt["window"].taskbarItemInfo.Overlay = "https://raw.githubusercontent.com/emad
 }
 function Startup {
 $UsersCount = "https://ittools-7d9fe-default-rtdb.firebaseio.com/message.json"
-$CPU = (Get-CimInstance Win32_Processor).ProcessorId
-ITT-ScriptBlock -ArgumentList $Debug $UsersCount $CPU -ScriptBlock {
+$CPU = (Get-CimInstance Win32_Processor).Name
+ITT-ScriptBlock -ArgumentList $Debug $CPU $UsersCount -ScriptBlock {
 param($Debug, $UsersCount)
 function Telegram {
 param (
@@ -12367,26 +12367,26 @@ $itt.event.FindName('closebtn').add_MouseLeftButtonDown({ $itt.event.Close() })
 $itt.event.FindName('DisablePopup').add_MouseLeftButtonDown({ DisablePopup; $itt.event.Close() })
 $itt.event.FindName('title').text = 'Changelog'.Trim()
 $itt.event.FindName('date').text = '03/01/2025'.Trim()
-$itt.event.FindName('preview2').add_MouseLeftButtonDown({
-Start-Process('https://github.com/emadadel4/itt')
-})
 $itt.event.FindName('RK').add_MouseLeftButtonDown({
 Start-Process('https://duckduckgo.com/?q=what+is+ramadan&t=brave&ia=web')
-})
-$itt.event.FindName('ps').add_MouseLeftButtonDown({
-Start-Process('https://www.palestinercs.org/en/Donation')
-})
-$itt.event.FindName('shell').add_MouseLeftButtonDown({
-Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
 })
 $itt.event.FindName('preview').add_MouseLeftButtonDown({
 Start-Process('https://github.com/emadadel4/itt')
 })
-$itt.event.FindName('esg').add_MouseLeftButtonDown({
+$itt.event.FindName('shell').add_MouseLeftButtonDown({
+Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
+})
+$itt.event.FindName('preview2').add_MouseLeftButtonDown({
 Start-Process('https://github.com/emadadel4/itt')
 })
 $itt.event.FindName('ytv').add_MouseLeftButtonDown({
 Start-Process('https://www.youtube.com/watch?v=QmO82OTsU5c')
+})
+$itt.event.FindName('esg').add_MouseLeftButtonDown({
+Start-Process('https://github.com/emadadel4/itt')
+})
+$itt.event.FindName('ps').add_MouseLeftButtonDown({
+Start-Process('https://www.palestinercs.org/en/Donation')
 })
 $itt.event.Add_PreViewKeyDown({ if ($_.Key -eq "Escape") { $itt.event.Close() } })
 $storedDate = [datetime]::ParseExact($itt.event.FindName('date').Text, 'MM/dd/yyyy', $null)
