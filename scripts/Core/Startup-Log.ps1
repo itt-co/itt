5f0c9deb3dbcd0777329985fec03de90da4d8737
ITT Community <167635892+itt-co@users.noreply.github.com>
2025-03-03 02:04:08 +0200

    Update Startup-Log.ps1
    Signed-off-by: ITT Community <167635892+itt-co@users.noreply.github.com>

--- scripts/Core/Startup-Log.ps1
+++ scripts/Core/Startup-Log.ps1
@@ -3,7 +3,7 @@ function Startup {
     $UsersCount = "https://ittools-7d9fe-default-rtdb.firebaseio.com/message.json"
     $CPU = (Get-CimInstance Win32_Processor).Name
 
-    ITT-ScriptBlock -ArgumentList @($Debug,$CPU,$UsersCount) -ScriptBlock {
+    ITT-ScriptBlock -ArgumentList @($CPU,$UsersCount) -Debug $Debug -ScriptBlock {
  
         param($Debug,$CPU,$UsersCount)
 
