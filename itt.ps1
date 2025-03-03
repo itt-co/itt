6b429bc692edbb774854d00945645f1af1be79e5
itt-co <167635892+itt-co@users.noreply.github.com>
2025-03-03 00:20:45 +0000

    update

--- itt.ps1
+++ itt.ps1
@@ -7249,7 +7249,7 @@ $itt["window"].taskbarItemInfo.Overlay = "https://raw.githubusercontent.com/emad
 function Startup {
 $UsersCount = "https://ittools-7d9fe-default-rtdb.firebaseio.com/message.json"
 $CPU = (Get-CimInstance Win32_Processor).Name
-ITT-ScriptBlock -ArgumentList @($CPU,$UsersCount) -Debug $Debug -ScriptBlock {
+ITT-ScriptBlock -ArgumentList @($CPU,$UsersCount) -Debug $debug -ScriptBlock {
 param($CPU,$UsersCount)
 function Telegram {
 param (
@@ -12368,26 +12368,26 @@ $itt.event.FindName('closebtn').add_MouseLeftButtonDown({ $itt.event.Close() })
 $itt.event.FindName('DisablePopup').add_MouseLeftButtonDown({ DisablePopup; $itt.event.Close() })
 $itt.event.FindName('title').text = 'Changelog'.Trim()
 $itt.event.FindName('date').text = '03/01/2025'.Trim()
+$itt.event.FindName('preview').add_MouseLeftButtonDown({
+Start-Process('https://github.com/emadadel4/itt')
+})
 $itt.event.FindName('ps').add_MouseLeftButtonDown({
 Start-Process('https://www.palestinercs.org/en/Donation')
-})
-$itt.event.FindName('ytv').add_MouseLeftButtonDown({
-Start-Process('https://www.youtube.com/watch?v=QmO82OTsU5c')
 })
 $itt.event.FindName('esg').add_MouseLeftButtonDown({
 Start-Process('https://github.com/emadadel4/itt')
 })
+$itt.event.FindName('ytv').add_MouseLeftButtonDown({
+Start-Process('https://www.youtube.com/watch?v=QmO82OTsU5c')
+})
 $itt.event.FindName('preview2').add_MouseLeftButtonDown({
 Start-Process('https://github.com/emadadel4/itt')
 })
-$itt.event.FindName('preview').add_MouseLeftButtonDown({
-Start-Process('https://github.com/emadadel4/itt')
+$itt.event.FindName('shell').add_MouseLeftButtonDown({
+Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
 })
 $itt.event.FindName('RK').add_MouseLeftButtonDown({
 Start-Process('https://duckduckgo.com/?q=what+is+ramadan&t=brave&ia=web')
-})
-$itt.event.FindName('shell').add_MouseLeftButtonDown({
-Start-Process('https://www.youtube.com/watch?v=nI7rUhWeOrA')
 })
 $itt.event.Add_PreViewKeyDown({ if ($_.Key -eq "Escape") { $itt.event.Close() } })
 $storedDate = [datetime]::ParseExact($itt.event.FindName('date').Text, 'MM/dd/yyyy', $null)
