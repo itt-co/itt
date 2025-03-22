d1b212e9bfaac36b3b8f9662147c697caa77033c
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-03-22 03:48:18 +0200

    Create restore point before applying tweaks

--- scripts/Core/2.Restore-Point.ps1
+++ scripts/Core/2.Restore-Point.ps1
@@ -3,9 +3,7 @@ function CreateRestorePoint {
     try {
         
         Add-Log -Message "Creating Restore point..." -Level "INFO"
-
         Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\SystemRestore" -Name "SystemRestorePointCreationFrequency" -Value 0 -Type DWord -Force
-
         powershell.exe -Command {
 
             $Date = Get-Date -Format "yyyyMMdd-hhmmss-tt"
@@ -17,7 +15,6 @@ function CreateRestorePoint {
 
         Set-ItemProperty -Path $itt.registryPath  -Name "backup" -Value 1 -Force
         Add-Log -Message "Created successfully" -Level "INFO"
-
     }
     catch {
         Add-Log -Message "Error: $_" -Level "ERROR"
