a039a3280ddf79b95f6cf231561904a3561d3d18
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-03-22 03:30:37 +0200

    Update 2.Restore-Point.ps1

--- scripts/Core/2.Restore-Point.ps1
+++ scripts/Core/2.Restore-Point.ps1
@@ -1,20 +1,23 @@
-function RestorePoint {
-    <#
-        .SYNOPSIS
-        Creates a system restore point on the local computer.
-        .DESCRIPTION
-        This function creates a system restore point using the Checkpoint-Computer cmdlet. It logs the process of creating the restore point and handles any errors that occur during the creation. If the restore point creation fails, an error message is displayed.
-        .EXAMPLE
-        RestorePoint
-        Creates a restore point and logs the success or failure of the operation.
-    #>
-    ITT-ScriptBlock -ScriptBlock {
-        Try {
-            Add-Log -Message "Creating Restore point..." -Level "INFO"
-            Start-Process powershell.exe -ArgumentList "-NoExit", "-Command `"Enable-ComputerRestore -Drive '$env:SystemDrive'; Checkpoint-Computer -Description 'ITT' -RestorePointType 'MODIFY_SETTINGS'; exit`"" -Wait -Verb RunAs
-            Add-Log -Message "Created successfully" -Level "INFO"
-        } Catch {
-            Add-Log -Message "An error occurred while enabling System Restore: $_" -Level "ERROR"
+function CreateRestorePoint {
+    
+    try {
+        
+        Add-Log -Message "Creating Restore point..." -Level "INFO"
+
+        powershell.exe -Command {
+
+            $Date = Get-Date -Format "yyyyMMdd-hhmmss-tt"
+            $RestorePointName = "ITT-$Date"
+            Enable-ComputerRestore -Drive $env:SystemDrive
+            Checkpoint-Computer -Description $RestorePointName -RestorePointType "MODIFY_SETTINGS"
+            Set-ItemProperty -Path $itt.registryPath  -Name "backup" -Value 1 -Force
+            exit
         }
+
+        Add-Log -Message "Created successfully" -Level "INFO"
+
+    }
+    catch {
+        Add-Log -Message "Error: $_" -Level "ERROR"
     }
 }
