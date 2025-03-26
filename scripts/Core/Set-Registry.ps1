01a194169670df3c7f6cae9aa66d9a7b751737cd
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-03-26 05:54:34 +0200

    Update Set-Registry.ps1

--- scripts/Core/Set-Registry.ps1
+++ scripts/Core/Set-Registry.ps1
@@ -1,24 +1,17 @@
 function Set-Registry {
+
     <#
         .SYNOPSIS
         Sets or creates a registry value at a specified path.
         .DESCRIPTION
-        This function sets a registry value at a given path. If the specified registry path does not exist, the function attempts to create the path and set the value. It handles different registry value types and includes error handling to manage potential issues during the process.
-        .PARAMETER Name
-        The name of the registry value to set or create. This parameter is required.
-        .PARAMETER Type
-        The type of the registry value. Common types include `String`, `DWord`, `QWord`, etc. This parameter is required.
-        .PARAMETER Path
-        The full path of the registry key where the value is to be set. This parameter is required.
-        .PARAMETER Value
-        The value to be set for the registry key. This parameter is required.
+        This function sets a registry value at a given path. If the specified registry path does not exist, the function attempts to create the path and set the value.
         .EXAMPLE
         Set-Registry -Name "EnableFeeds" -Type "DWord" -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" -Value 0
         Sets the registry value named "EnableFeeds" to 0 (DWORD) under "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds". If the path does not exist, it attempts to create it.
     #>
-    param (
-        [array]$tweak
-    )
+
+    param ([array]$tweak)
+    
     try {
         if(!(Test-Path 'HKU:\')) {New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS}
         $tweak | ForEach-Object {
