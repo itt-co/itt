50c59bb671ccd7cb7e35bbb8e222d87c83da2593
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-03-26 06:15:11 +0200

    Update Set-Theme.ps1

--- scripts/UI/Set-Theme.ps1
+++ scripts/UI/Set-Theme.ps1
@@ -10,6 +10,7 @@ function SwitchToSystem {
     #>
 
     try {
+        $appsTheme = Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme"
         $theme = if ($AppsTheme -eq "0") { "Dark" } elseif ($AppsTheme -eq "1") { "Light" } else { Write-Host "Unknown theme: $AppsTheme"; return }
         $itt['window'].Resources.MergedDictionaries.Add($itt['window'].FindResource($theme))
         Set-ItemProperty -Path $itt.registryPath -Name "Theme" -Value "default" -Force
