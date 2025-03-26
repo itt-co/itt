bbed0c60bbd92023869d2735145495b047a5d480
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-03-26 06:39:15 +0200

    IDM Trail reset
    repo
    https://github.com/J2TEAM/idm-trial-reset

--- scripts/Invoke/Invoke-Button.ps1
+++ scripts/Invoke/Invoke-Button.ps1
@@ -194,6 +194,13 @@ function Invoke-Button {
                 Debug-Message $action
             # debug end
         }
+        "idm" {
+            Add-Log -Message "Running IDM Activation..." -Level "info"
+            ITT-ScriptBlock -ScriptBlock {curl.exe -L -o $env:TEMP\\IDM_Trial_Reset.exe "https://github.com/itt-co/itt-packages/raw/refs/heads/main/automation/idm-trial-reset/IDM%20Trial%20Reset.exe" && cmd /c $env:TEMP\\IDM_Trial_Reset.exe}
+            # debug start
+                Debug-Message $action
+            # debug end
+        }
         "neat" {
             Start-Process "https://addons.mozilla.org/en-US/firefox/addon/neatdownloadmanager-extension/" 
             # debug start
