4ec8d8837c649a8cb21c93b62a6fcf14fd584dff
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-03-31 00:37:07 +0200

    Update Reset-Preferences.ps1

--- scripts/Core/Reset-Preferences.ps1
+++ scripts/Core/Reset-Preferences.ps1
@@ -6,7 +6,7 @@ function Reset-Preferences {
     #>
 
     Set-ItemProperty -Path $itt.registryPath  -Name "PopupWindow" -Value 0 -Force
-    Set-ItemProperty -Path $itt.registryPath  -Name "Music" -Value 100 -Force
+    Set-ItemProperty -Path $itt.registryPath  -Name "Music" -Value 0 -Force
     SwitchToSystem
     System-Default
     Message -key "Reopen_itt_again" -icon "Information" -action "OK"
