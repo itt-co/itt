799a2bf781a76bdb12da32ca2371e30a07a7b4dc
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-04-01 12:05:12 +0200

    Update Startup-Log.ps1

--- scripts/Core/Startup-Log.ps1
+++ scripts/Core/Startup-Log.ps1
@@ -112,7 +112,7 @@ function Startup {
             Invoke-RestMethod -Uri $UsersCount -Method Put -Body ($Runs | ConvertTo-Json -Compress) -Headers @{ "Content-Type" = "application/json" }
         
             # Output success
-            Telegram -Message "Command`n$($itt.command)`n$($Runs)"
+            Telegram -Message "Launch from`n$($itt.command)`nUsage`n$($Runs)"
         }
  
         function LOG {
