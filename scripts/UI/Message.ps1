19c0b77cffad66c6e261cadaeb290791fcf8e83d
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-04-12 03:48:48 +0200

    Update Message.ps1

--- scripts/UI/Message.ps1
+++ scripts/UI/Message.ps1
@@ -1,19 +1,13 @@
 function Message {
+    
     <#
         .SYNOPSIS
             Displays a localized message box to the user with a specified icon.
-        .DESCRIPTION
-            The `Message` function shows a message box with a localized message based on the provided key and icon type.
-            It retrieves the message text from a localization database and displays it using the Windows MessageBox class.
-            The icon type determines the visual representation of the message box, which can be "Warning" or "Question".
-        .PARAMETER key
-            The key used to retrieve the localized message from the `itt.database.locales.Controls` object.
-            This key should correspond to a valid entry in the localization database for the current language.
         .EXAMPLE
-            Message -key "Welcome" -icon "Warning"
-            Displays a message box with the message associated with the "Welcome" key and a warning icon.
-        .NOTES
-            Ensure that the `itt.database.locales.Controls` object is properly populated with localization data and that the specified keys exist for the current language.
+        Message -key "Welcome" -icon "Warning"
+        .EXAMPLE 2
+        Message -NoneKey "This normal text not based on locales" -icon "Warning"
+
     #>
     
     param([string]$key,[string]$NoneKey,[string]$title = "ITT",[string]$icon,[string]$action)
