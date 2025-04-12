6a00c32ed9e9701594d4abfc23af563eb1a37124
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-04-12 03:48:50 +0200

    Update Notify.ps1

--- scripts/UI/Notify.ps1
+++ scripts/UI/Notify.ps1
@@ -1,15 +1,13 @@
 function Notify {
+
     <#
         .SYNOPSIS
         Displays a balloon tip notification in the system tray with a customizable title, message, icon, and duration.
-        .DESCRIPTION
-        The `Notify` function creates a balloon tip notification using the system tray's notification area. 
-        This function is useful for displaying temporary alerts or messages to the user. 
-        It allows you to specify the notification's title, message, icon type, and how long the balloon tip should be displayed.
         .EXAMPLE
         Notify -title "ITT" -msg "Hello world!" -icon "Information" -time 3000
         Displays a notification balloon with the title "ITT" and the message "Hello world!" with an informational icon for 3 seconds.
     #>
+    
     param(
         [string]$title,
         [string]$msg,
