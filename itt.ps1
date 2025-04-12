54c87383687af75a6302eceff5afe2ca63657450
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-04-12 09:03:53 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -10300,7 +10300,7 @@ $functions = @(
 'Disable-Service', 'Uninstall-AppxPackage', 'Finish', 'Message',
 'Notify', 'UpdateUI', 'Install-ITTAChoco',
 'ExecuteCommand', 'Set-Registry', 'Set-Taskbar',
-'Refresh-Explorer', 'Remove-ScheduledTasks','CreateRestorePoint','Statusbar'
+'Refresh-Explorer', 'Remove-ScheduledTasks','CreateRestorePoint','Set-Taskbar'
 )
 foreach ($func in $functions) {
 $command = Get-Command $func -ErrorAction SilentlyContinue
