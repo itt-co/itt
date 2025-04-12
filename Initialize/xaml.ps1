b2a0d4f80fbc5427c147f2e67228da3bffca37ce
Emad Adel <167635892+itt-co@users.noreply.github.com>
2025-04-13 00:51:09 +0200

    Update xaml.ps1

--- Initialize/xaml.ps1
+++ Initialize/xaml.ps1
@@ -189,6 +189,13 @@ $itt.applyText = $itt["window"].FindName("applyText")
 $itt.applyIcon = $itt["window"].FindName("applyIcon")
 $itt.QuoteIcon = $itt["window"].FindName("QuoteIcon")
 
+
+# Cacheing Applications & Tweaks for quick respnse
+$tweaksDict = @{}
+foreach ($tweak in $itt.database.Tweaks) {
+    $tweaksDict[$tweak.Name] = $tweak
+}
+
 #===========================================================================
 #endregion Initialize WPF Controls
 #===========================================================================
