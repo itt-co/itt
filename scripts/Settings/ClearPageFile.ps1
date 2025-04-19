aaa7b7297d188f67e7b0d16f1a59a9f64654e098
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-04-19 03:15:36 +0200

    Update ClearPageFile.ps1

--- scripts/Settings/ClearPageFile.ps1
+++ scripts/Settings/ClearPageFile.ps1
@@ -13,11 +13,11 @@ function Invoke-ClearPageFile {
     Try {
         if ($Enabled -eq $false) {
             $value = 1
-            Add-Log -Message "Show End Task on taskbar" -Level "Apply"
+            Add-Log -Message "Show End Task on taskbar" -Level "info"
         }
         else {
             $value = 0
-            Add-Log -Message "Disable End Task on taskbar" -Level "Disabled"
+            Add-Log -Message "Disable End Task on taskbar" -Level "info"
         }
         Set-ItemProperty -Path $Path -Name $name -Value $value -ErrorAction Stop
     }
