3991736d38fe326ef69ea38f1598fc28f90c3d9d
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-04-19 03:15:43 +0200

    Update Disable-DriversUpdate.ps1

--- scripts/Settings/Disable-DriversUpdate.ps1
+++ scripts/Settings/Disable-DriversUpdate.ps1
@@ -26,11 +26,11 @@ function Invoke-DisableAutoDrivers {
         Try{
             if ($Enabled -eq $false){
                 $value = 1
-                Add-Log -Message "Enabled auto drivers update" -Level "Apply"
+                Add-Log -Message "Enabled auto drivers update" -Level "info"
             }
             else {
                 $value = 0
-                Add-Log -Message "Disabled auto drivers update" -Level "Disabled"
+                Add-Log -Message "Disabled auto drivers update" -Level "info"
             }
         Set-ItemProperty -Path $Path -Name $name -Value $value -ErrorAction Stop
         }
