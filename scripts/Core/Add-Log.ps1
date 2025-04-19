cb27be36c057f2fe8329850c59dfae80d982e7c3
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-04-19 03:18:08 +0200

    Update Add-Log.ps1

--- scripts/Core/Add-Log.ps1
+++ scripts/Core/Add-Log.ps1
@@ -9,7 +9,7 @@ function Add-Log {
 
     $level = $Level.ToUpper()
     $colorMap = @{ INFO="White"; WARNING="Yellow"; ERROR="Red"; INSTALLED="White"; APPLY="White"; DEBUG="Yellow" }
-    $iconMap  = @{ INFO="[+]"; WARNING="[!]"; ERROR="[X]"; DEFAULT=$null; DEBUG="[Debug]"; ITT="[ITT]"; Chocolatey="Chocolatey"; Winget="[Winget]" }
+    $iconMap  = @{ INFO="[+]"; WARNING="[!]"; ERROR="[X]"; DEFAULT=$null; DEBUG="[Debug]"; ITT="[ITT]"; Chocolatey="[Chocolatey]"; Winget="[Winget]" }
 
     $color = if ($colorMap.ContainsKey($level)) { $colorMap[$level] } else { "White" }
     $icon  = if ($iconMap.ContainsKey($level)) { $iconMap[$level] } else { "i" }
