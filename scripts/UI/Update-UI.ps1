6867b8d428c4e71e3537c98b38214654dc8f5b43
Emad Adel <emadadel1920@gmail.com>
2025-04-27 12:17:15 +0300

    Update Update-UI.ps1
    Signed-off-by: Emad Adel <emadadel1920@gmail.com>

--- scripts/UI/Update-UI.ps1
+++ scripts/UI/Update-UI.ps1
@@ -3,18 +3,16 @@ function UpdateUI {
     <#
         .SYNOPSIS
         Updates the user interface elements, including a button's width, text, and associated icons.
-        .DESCRIPTION
-        The `UpdateUI` function is designed to modify various UI components within the application. 
-        It updates the width and text of a specified button, changes the text of a related text block, and sets the icon for another text block. 
-        This function is typically used to reflect different states of the application, such as during installations or other processes.
     #>
 
     param([string]$Button,[string]$Content,[string]$Width = "140")
     
     $key = $itt.database.locales.Controls.$($itt.Language).$Content
 
+    $text = if (-not $key) { $Content } else { $key }
+
     $itt['window'].Dispatcher.Invoke([Action]{
         $itt.$Button.Width = $Width
-        $itt.$Button.Content = "$key"
+        $itt.$Button.Content = "$text"
     })
 }
