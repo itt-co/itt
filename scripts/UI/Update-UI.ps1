4b5e445b1f98de6d37ae69a55680c8f26377624e
Emad Adel <emadadel1920@gmail.com>
2025-04-27 12:21:42 +0300

    Update Update-UI.ps1
    Signed-off-by: Emad Adel <emadadel1920@gmail.com>

--- scripts/UI/Update-UI.ps1
+++ scripts/UI/Update-UI.ps1
@@ -1,19 +1,13 @@
 function UpdateUI {
-    
+
     <#
         .SYNOPSIS
         Updates the user interface elements, including a button's width, text, and associated icons.
     #>
-
-    param([string]$Button,[string]$Content,[string]$Width = "140")
     
-    $key = $itt.database.locales.Controls.$($itt.Language).$Content
-    
-    # Check if the localized string (key) exists in the locales
-    $text = if (-not $key) { $Content } else { $key }
-
+    param([string]$Button, [string]$Content, [string]$Width = "140")
     $itt['window'].Dispatcher.Invoke([Action]{
         $itt.$Button.Width = $Width
-        $itt.$Button.Content = "$text"
+        $itt.$Button.Content = $itt.database.locales.Controls.$($itt.Language).$Content -or $Content
     })
 }
