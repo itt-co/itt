e94ca79c56f73092464ad9ef8a11df2ff54cf279
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2025-04-29 07:00:19 +0300

    Update Add-Log.ps1

--- scripts/Core/Add-Log.ps1
+++ scripts/Core/Add-Log.ps1
@@ -5,11 +5,11 @@ function Add-Log {
         Custom Write-Host Display Text with icon and name
     #>
 
-    param ([string]$Message, [string]$Level = "INFO")
+    param ([string]$Message, [string]$Level = "Default")
 
     $level = $Level.ToUpper()
     $colorMap = @{ INFO="White"; WARNING="Yellow"; ERROR="Red"; INSTALLED="White"; APPLY="White"; DEBUG="Yellow" }
-    $iconMap  = @{ INFO="[+]"; WARNING="[!]"; ERROR="[X]"; DEFAULT=$null; DEBUG="[Debug]"; ITT="[ITT]"; Chocolatey="[Chocolatey]"; Winget="[Winget]" }
+    $iconMap  = @{ INFO="[+]"; WARNING="[!]"; ERROR="[X]"; DEFAULT=""; DEBUG="[Debug]"; ITT="[ITT]"; Chocolatey="[Chocolatey]"; Winget="[Winget]" }
 
     $color = if ($colorMap.ContainsKey($level)) { $colorMap[$level] } else { "White" }
     $icon  = if ($iconMap.ContainsKey($level)) { $iconMap[$level] } else { "i" }
