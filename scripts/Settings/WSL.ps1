42ef3770138927e92c06674c99e1b26af908bc1e
ITT Community <19177373+emadadel4@users.noreply.github.com>
2025-06-14 12:16:44 +0300

    Fix

--- scripts/Settings/WSL.ps1
+++ scripts/Settings/WSL.ps1
@@ -9,14 +9,16 @@ function Invoke-WSL {
 
     Try{
         if ($Enabled -eq $false){
-            Add-Log -Message "WSL2 disabled" -Level "info"
-            dism.exe /online /disable-feature /featurename:Microsoft-Windows-Subsystem-Linux /norestart
-            dism.exe /online /disable-feature /featurename:VirtualMachinePlatform /norestart
+            Add-Log -Message "Enabling WSL2..." -Level "info"
+            Start-Process powershell -ArgumentList 'dism.exe /online /enable-feature /featurename:"Microsoft-Windows-Subsystem-Linux" /all /norestart' -Verb RunAs
+            Start-Process powershell -ArgumentList 'dism.exe /online /enable-feature /featurename:"VirtualMachinePlatform" /all /norestart' -Verb RunAs
+            Add-Log -Message "Restart required" -Level "info"
         }
         else {
-            Add-Log -Message "WSL2 enabled" -Level "info"
-            dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-            dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+            Add-Log -Message "Disabling WSL2..." -Level "info"
+            Start-Process powershell -ArgumentList 'dism.exe /online /disable-feature /featurename:"Microsoft-Windows-Subsystem-Linux" /norestart' -Verb RunAs
+            Start-Process powershell -ArgumentList 'dism.exe /online /disable-feature /featurename:"VirtualMachinePlatform" /norestart' -Verb RunAs
+            Add-Log -Message "Restart required" -Level "info"
         }
     }
 
