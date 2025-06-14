e3730fbaa936986cb335f8a77d80f7c07abfe2c3
ITT Community <19177373+emadadel4@users.noreply.github.com>
2025-06-14 11:10:35 +0300

    Update Update-UI.ps1

--- scripts/UI/Update-UI.ps1
+++ scripts/UI/Update-UI.ps1
@@ -5,10 +5,16 @@ function UpdateUI {
         Update button's content width, text.
     #>
 
-    param([string]$Button,[string]$Content,[string]$Width = "140")
+    param([string]$Button,[string]$Content,[string]$NonKey,[string]$Width = "140")
 
     $itt['window'].Dispatcher.Invoke([Action]{
         $itt.$Button.Width = $Width
-        $itt.$Button.Content = $itt.database.locales.Controls.$($itt.Language).$Content
+
+        if($Content)
+        {
+            $itt.$Button.Content = $itt.database.locales.Controls.$($itt.Language).$Content
+        }else{
+            $itt.$Button.Content = $NonKey
+        }
     })
 }
